d19e942cc7844af33c1d020d06058de274179b60
valerii26 <svv2602@gmail.com>
2023-05-20 17:24:33 +0300

    исправлен string_processor

--- lib/servis_pdf/string_processor.rb
+++ lib/servis_pdf/string_processor.rb
@@ -23,27 +23,27 @@ class StringProcessor
   end
 
 end
-
-h ={
-  "23"=>{"row1"=>"  385/55R22.5 20 NEO AllroadsT2 TL                               160K                 US$222.00",
-         "row2"=>"             380/55R22.5 20 NEO AllroadsT TLS  20   PCS   US$222.00    US$4,440.00",
-         "sum"=>5,
-         "sum_all"=>80.0,
-         "line_num"=>30},
-  "25"=>{"row1"=>"  175/55R17 20 NEO AllroadsT2 TL                                         US$222.00",
-         "row2"=>"             385/55R22.5 20 NEO AllroadsT2 TL  20   PCS   US$292.00    US$4,840.00",
-         "sum"=>5,
-         "sum_all"=>80.0,
-         "line_num"=>30}
-}
-
-
-h.each do |key, hash_in|
-  processor = StringProcessor.new(hash_in["row1"])
-  result = processor.mark_common_words_with_html(hash_in["row2"])
-  hash_in["row1"] = result[:str1]
-  hash_in["row2"] = result[:str2]
-end
-
-puts h.inspect
+#
+# h ={
+#   "23"=>{"row1"=>"  385/55R22.5 20 NEO AllroadsT2 TL                               160K                 US$222.00",
+#          "row2"=>"             380/55R22.5 20 NEO AllroadsT TLS  20   PCS   US$222.00    US$4,440.00",
+#          "sum"=>5,
+#          "sum_all"=>80.0,
+#          "line_num"=>30},
+#   "25"=>{"row1"=>"  175/55R17 20 NEO AllroadsT2 TL                                         US$222.00",
+#          "row2"=>"             385/55R22.5 20 NEO AllroadsT2 TL  20   PCS   US$292.00    US$4,840.00",
+#          "sum"=>5,
+#          "sum_all"=>80.0,
+#          "line_num"=>30}
+# }
+#
+#
+# h.each do |key, hash_in|
+#   processor = StringProcessor.new(hash_in["row1"])
+#   result = processor.mark_common_words_with_html(hash_in["row2"])
+#   hash_in["row1"] = result[:str1]
+#   hash_in["row2"] = result[:str2]
+# end
+#
+# puts h.inspect
 
