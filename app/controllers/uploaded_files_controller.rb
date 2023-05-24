f73af0fe868515f449f416c2baa204841609be63
valerii26 <svv2602@gmail.com>
2023-05-25 00:22:40 +0300

    Доработан DocPdfOCR и UploadedFilesController

--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -1,3 +1,8 @@
+
+require 'servis_pdf/doc_pdf_ocr'
+# метка очистки
+
+
 class UploadedFilesController < ApplicationController
   before_action :set_project
   before_action :set_file, only: [:show, :destroy]
@@ -13,24 +18,71 @@ class UploadedFilesController < ApplicationController
     @uploaded_file = @project.uploaded_files.build
   end
 
-  def create
-    @uploaded_file = @project.uploaded_files.build(file_params)
-    @uploaded_file.name = file_params[:content].original_filename
+  # def create
+  #   @uploaded_file = @project.uploaded_files.build(file_params)
+  #   @uploaded_file.name = file_params[:content].original_filename
+  #
+  #
+  #   if @uploaded_file.save
+  #     if file_params[:content].content_type != "application/pdf"
+  #       flash[:danger] = "Файл без расширения PDF добавлен в проект.  Для него не будет доступно сравнение."
+  #     else
+  #       flash[:success] = 'Файл успешно добавлен в проект.'
+  #     end
+  #
+  #     redirect_to edit_project_path(@project)
+  #   else
+  #     flash.now[:info] = 'Файл не был добавлен в проект.'
+  #     render :new
+  #   end
+  # end
 
+
+  def create
+    file_extension = File.extname(file_params[:content].original_filename).downcase
+    content_file = file_params[:content]
+
+    if file_extension == ".pdf"
+      file_path = content_file.path # Получаем путь к загруженному файлу
+
+      obj = DocPdfOCR.new(file_path) # Создаем экземпляр класса DocPdfOCR
+
+      if obj.create_pdf_with_ocr == false # Проверяем результат создания PDF с распознанным текстом
+        @uploaded_file = @project.uploaded_files.build(content: content_file) # Создаем экземпляр UploadedFile с исходным загруженным файлом
+      else
+        converted_pdf_path = obj.create_pdf_with_ocr # Получаем путь к обработанному PDF
+        content_blob = ActiveStorage::Blob.create_and_upload!(
+          io: File.open(converted_pdf_path),
+          filename: File.basename(converted_pdf_path),
+          content_type: "application/pdf"
+        )
+        @uploaded_file = @project.uploaded_files.build(content: content_blob) # Создаем экземпляр UploadedFile с обработанным файлом
+      end
+    else
+      @uploaded_file = @project.uploaded_files.build(content: content_file) # Создаем экземпляр UploadedFile с исходным загруженным файлом
+    end
+
+    @uploaded_file.name = content_file.original_filename
 
     if @uploaded_file.save
-      if file_params[:content].content_type != "application/pdf"
-        flash[:danger] = "Файл без расширения PDF добавлен в проект.  Для него не будет доступно сравнение."
+
+      if file_extension == ".pdf" && obj.create_pdf_with_ocr != false
+        flash[:success] = 'Файл успешно добавлен в проект. Загружен файл с распознанным текстом.'
       else
         flash[:success] = 'Файл успешно добавлен в проект.'
       end
 
+      DocPdfOCR.remove_files_from_dir
       redirect_to edit_project_path(@project)
     else
       flash.now[:info] = 'Файл не был добавлен в проект.'
       render :new
     end
   end
+
+
+
+
 
   def destroy
     @uploaded_file.destroy
--- lib/servis_pdf/doc_pdf_ocr.rb
+++ lib/servis_pdf/doc_pdf_ocr.rb
@@ -1,19 +1,42 @@
 require 'rtesseract'
 require 'prawn'
+require 'pdf/reader'
+require 'fileutils'
 
 class DocPdfOCR
-  def basename_path(file_path)
-    File.dirname(file_path) + '/' + File.basename(file_path, '.*') + '.'
+  def initialize(file_path, id_user = 1)
+    @file_path = file_path
+    @id_user = id_user
   end
 
-  def convert_pdf_to_img(file_path, extension = "jpg")
-    file_extension = File.extname(file_path).downcase
+  def self.remove_files_from_dir(id_user = 1)
+    folder_path = "app/assets/temp/#{id_user}"
+    if File.directory?(folder_path)
+      # Удаляем все файлы в папке
+      FileUtils.rm_rf(Dir.glob("#{folder_path}/*"))
+      puts "Все файлы были удалены из папки #{folder_path}"
+    else
+      puts "Указанная папка #{folder_path} не существует"
+    end
+  end
+
+  def basename_path
+    file_directory = "app/assets/temp/#{@id_user}"
+    unless File.directory?(file_directory)
+      FileUtils.mkdir_p(file_directory)
+    end
+    file_directory + "/" + File.basename(@file_path, '.*') + '.'
+  end
+
+  def convert_pdf_to_img(extension = "jpg")
+    file_extension = File.extname(@file_path).downcase
 
     if file_extension == ".pdf"
       hash = { "jpeg" => "jpeg", "png" => "png", "jpg" => "jpeg" }
       if hash.key?(extension)
-        img_path = basename_path(file_path) + extension
-        result = `pdftoppm -#{hash[extension]} #{file_path} #{img_path} 2>&1`
+        img_path = basename_path + extension
+        # result = `pdftoppm -#{hash[extension]} #{file_path} #{img_path} 2>&1`
+        result = system("pdftoppm -#{hash[extension]} #{@file_path} #{img_path} 2>&1")
         if $?.success?
           puts "PDF успешно преобразован в изображение"
         else
@@ -25,23 +48,58 @@ class DocPdfOCR
     else
       puts "Неизвестный формат входного файла"
     end
+    return img_path
   end
 
-  def create_pdf_with_ocr(file_path)
-    pdf_path = basename_path(file_path) + 'pdf'
-    image_text = RTesseract.new(file_path).to_s
-    image_text = image_text.gsub(/\n /, '').gsub(/\n+/, "\n").gsub(/_+/," ").gsub(/US(S|s)/,"US$")
-    image_text = image_text.gsub(/\$+/, '$')
-    image_text = image_text.gsub(/RI(?=\d|T)/, 'R1').gsub(/(?<=(R1))T/,'7').gsub(/(?<=(\d ))(i|I|1)s(?= \|)/,'18')
-    puts image_text.inspect
+  def create_pdf_with_ocr
+    # puts convert_pdf_to_img(file_path).gsub(/jpg/,"jpg-1.jpg")
+    unless contains_text?
+      file_path = convert_pdf_to_img.gsub(/jpg/,"jpg-1.jpg")
+      pdf_path = basename_path + 'pdf'
+      image_text = RTesseract.new(file_path).to_s
+      # puts image_text
+      image_text = image_text.gsub(/\n /, '').gsub(/\n+/, "\n").gsub(/_+/," ").gsub(/US(S|s)/,"US$")
+      image_text = image_text.gsub(/\$+/, '$')
+      image_text = image_text.gsub(/RI(?=\d|T)/, 'R1').gsub(/(?<=(R1))T/,'7').gsub(/(?<=(\d ))(i|I|1)s(?= \|)/,'18')
+      # puts image_text.inspect
 
-    Prawn::Document.generate(pdf_path) do
-      text image_text
+      Prawn::Document.generate(pdf_path) do
+        text image_text
+      end
+
+      # puts "Файл PDF с распознанным текстом успешно создан: #{pdf_path}"
+      pdf_path
+    else
+      false
     end
 
-    puts "Файл PDF с распознанным текстом успешно создан: #{pdf_path}"
+  end
+
+  def contains_text?
+    contains_text = false
+
+      reader = PDF::Reader.new(@file_path)
+
+      reader.pages.each do |page|
+        text = page.text
+        if text.match?(/\S/) # Проверяем, содержит ли текст страницы непустые символы, отличные от пробелов
+          contains_text = true
+          break
+        end
+      end
+
+    contains_text
   end
 
 end
+
+
+# file_path = "/home/user/Рабочий стол/новая_таможня/invoice.pdf"
+# file_path = 'lib/servis_pdf/inv3a.pdf'
+# file_path = 'lib/servis_pdf/invoice.pdf'
+# doc_pdf_ocr = DocPdfOCR.new(file_path)
+# puts doc_pdf_ocr.contains_text?
+# puts doc_pdf_ocr.create_pdf_with_ocr
+
 
 
